ddb3101ead987d1f974a0176a7205b760cdc1c5f
AnkurYadav1999 <100476705+AnkurYadav1999@users.noreply.github.com>
2022-06-24 13:58:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,215 @@
+// console.log("Hello world");
+
+// var myName = "Ankur Yadav";
+
+// var myAge = 22;
+
+// console.log(myName);
+// console.log(myAge);
+
+
+
+/***Data Types in Javascript***/
+
+// Number
+// String
+// Boolean
+// Undefined
+// BigInt
+// Symbol
+
+// var mname = "Ankur";
+// var age = 22;
+// var isStudying = true;
+// var Placed;
+
+// console.log(typeof mname);
+// console.log(typeof age);
+// console.log(typeof isStudying);
+// console.log(typeof Placed);
+
+
+//--------- Challenge--------//
+
+// console.log(10+"20");
+// console.log(9-"5");
+// console.log("Java"+"Script");
+// console.log(" "+" ");
+// console.log(" "+0);
+// console.log("vinod" - "thapa");
+// console.log(true+true);
+// console.log(true+false);
+// console.log(false+true);
+// console.log(false-true);
+
+
+//-----Null and Undefined ------- //
+
+
+// var boss = null;
+// var money;
+
+// console.log(boss);
+// console.log(typeof boss);
+
+// console.log(money);
+// console.log(typeof money);
+
+
+
+// -------NaN---------//
+
+// console.log(NaN===NaN);
+
+
+// Challenge Time ----//
+
+// console.log(3**3);
+
+// console.log(14 + "14");
+
+
+// var a = 10;
+// var b = 15;
+
+// console.log("Value of a before "+a);
+// console.log("Value of b before "+b);
+
+// a=a+b;
+// b=a-b;
+// a=a-b;
+
+// console.log("The value of a after swap is "+a);
+// console.log("The value of b after swap is "+b);
+
+
+
+
+// ----------Control Statements and Loops -----------//
+
+// var tomr = "rain";
+
+// if(tomr=='rain'){
+//     console.log('take raincoat');
+// }
+// else{
+//     console.log('no need for raincoat');
+// }
+
+// ------ Challenge Time ---------------//
+
+// var year = 1900;
+
+// if(year%400==0){
+//     console.log("Its a Leap Year");
+// }
+// else if(year%100 == 0){
+//     console.log(" Not a Leap Year");
+// }
+// else if(year%4 == 0){
+//     console.log("A leap year");
+// }
+// else{
+//     console.log("Not a leap year")
+// }
+
+
+
+// -----------Truthy and Falsy Values -------//
+
+// if(null){
+//     console.log("we loose the game");
+// }
+// else{
+//     console.log("we won the game");
+// }
+
+
+
+// Ternary Operator
+
+// var age =22;
+//  var result = (age>18)?"You can vote":"You cannot vote";
+
+// console.log(result)
+
+
+
+// while Loop
+// var i=1;
+
+// while(i<=10){
+//     console.log(i)
+//     i++;
+// }
+
+
+
+// Do While Loop
+
+// var i = 1;
+
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<=20);
+
+
+// For Loop
+
+// var table = 15 ;
+// for(var i=1;i<=10;i++){
+//     console.log(i*table);
+// }
+
+
+
+// ------ Functions ----------
+
+// function sum(a,b){
+
+//     console.log(a+b);
+// }
+
+// sum(100,200);
+// sum(180,600);
+// sum(900,1200);
+
+
+// Function expression and Anonymous Expression ---------
+
+// const sum = function(a,b){
+//     return a+b;
+// }
+
+// var result = sum(10,20);
+
+// console.log(result);
+
+
+
+// Modern Javascript ES6
+
+// Let vs const vs var
+
+// Arrow Functions
+
+
+// const sum = (a,b)=>{
+//     console.log(a+b);
+// }
+
+// sum(20,3);
+
+
+
+// Arrays in Javascript -------//
+
+var fruits = ["Apple","Mango","Banana","Orange","Kiwi"];
+
+console.log(fruits[1]);
+
+
+
+
+
